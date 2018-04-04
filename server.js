fe0d54c58260303e2ba2f6b229ec0035649a3f74
amany9000 <201651007@iiitvadodara.ac.in>
2018-04-04 17:21:49 +0530

    added hadlebars in the home page

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ app.set("view engine",'hbs');
 app.use(express.static(__dirname + "/public"));
 
 app.get('/',(req,res) => {
-	res.send("<h1>Wellcome, Mr. Wayne!!!<h1>");
+	res.render("home.hbs",{
+		heading:"Wellcome, Mr. Wayne!!!",
+		body   :  "Coming here was a grave mistake by you, I am afraid."
+	});
 });
 
 app.get("/about",(req,resp)=>{
-	resp.render("About Us",{
+	resp.render("about.hbs",{
 		heading : "About Us",
 		body 	: "League of Shadows has been a top level saboteur for a millennium. We sacked Rome, loaded the merchent ships with plague rats, we burnt London. Whenever an emprire reaches the pinnacle of its decadence, we come back to restore the order."	
 	})
@@ -22,3 +25,4 @@ app.get("/rooot",(req,res) => {
 });
 app.listen(3000);
 console.log("Server running and listening to port 3000");
+
